27867c84ea249c4498214f53f89f4da5854f68f7
davitovmasyan <davitovmasyan@gmail.com>
2020-12-17 00:51:25 +0400

    Classwork 1

---
+++ Classwork_1/random_stuff.py
@@ -0,0 +1,30 @@
+num = input('Enter a num: ')
+
+num = int(num)
+
+a1 = input('Enter the first member of progression: ')
+a2 = input('Enter the second member of progression: ')
+n = input('Enter the N: ')
+
+if 99 < num < 1000:
+    print(num % 10)
+    print(num // 10 % 10)
+    print(num // 100 % 10)
+
+
+# if num > 99:
+#     print('In the middle')
+#     if num < 1000:
+#         print(num)
+#     else:
+#         print('Bigger than 999')
+# else:
+#     print('Less than 100')
+
+# first_name = input('Enter your last name: ')
+# last_name = input('Enter your last name: ')
+#
+# first_name = first_name.title()
+# last_name = last_name.title()
+#
+# print(first_name, last_name)
